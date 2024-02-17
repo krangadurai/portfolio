68fe8f6696f2769810870ecd29455dabb0681a9c
krangadurai <rangadurai27@gmail.com>
2024-02-17 14:53:51 +0530

    depencey add tailwind and framer-motion react-routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
 export default {
     content: [
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                mont: ['Montserrat', ...fontFamily.sans],
+            },
+            colors: {
+                dark: "#1b1b1b",
+                light: "#f5f5f5",
+                primary: "#B63E96", // 240,86,199
+                primaryDark: "#58E6D9", // 80,230,217
+            }
+        },
     },
     plugins: [],
 }
